b1ca5ac8b3dc24ef4bbd85206450981c73eb684d
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-04 14:09:11 +0100

    GameLoop init update

--- SDL Game/GameLoop.cpp
+++ SDL Game/GameLoop.cpp
@@ -1,5 +1,6 @@
 #include "GameLoop.h"
 
+
 GameLoop::GameLoop()
 {
 
@@ -14,7 +15,34 @@ GameLoop::~GameLoop()
 
 void GameLoop::init(const char* title, int xPos, int yPos, int width, int height, bool fullscreen)
 {
+	int flags = 0;
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
 
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		cout << "Subsystems Initialized!" << endl;
+
+		window = SDL_CreateWindow(title, xPos, yPos, width, height, flags);
+		if (window)
+		{
+			cout << "Window Created!" << endl;
+		}
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			cout << "Renderer Created!" << endl;
+
+		}
+
+		isRunning = true;
+	}
+	else
+	{
+		isRunning = false;
+	}
 }
 void GameLoop::handleEvents()
 {
