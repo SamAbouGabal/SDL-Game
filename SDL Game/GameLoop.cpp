224907c5691bb16896fce972d9eadeb8f95222fe
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-04 13:52:41 +0100

    Update to GameLoop cpp

--- SDL Game/GameLoop.cpp
+++ SDL Game/GameLoop.cpp
@@ -11,3 +11,27 @@ GameLoop::~GameLoop()
 
 
 }
+
+void GameLoop::init(const char* title, int xPos, int yPos, int width, int height, bool fullscreen)
+{
+
+}
+void GameLoop::handleEvents()
+{
+
+}
+
+void GameLoop::update()
+{
+
+}
+
+void GameLoop::render()
+{
+
+}
+
+void GameLoop::clean()
+{
+
+}
