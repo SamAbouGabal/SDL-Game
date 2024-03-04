36401406b773a93b975b316166c18cabcecdd036
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-04 14:23:12 +0100

    Update to main.cpp Game window now displays

--- SDL Game/main.cpp
+++ SDL Game/main.cpp
@@ -1,10 +1,24 @@
 #include "SDL.h"
+#include "GameLoop.h"
 
 using namespace std;
 
+GameLoop* gameLoop = nullptr;
+
 int main(int argc, char* argv[]) 
 {
+	gameLoop = new GameLoop();
+	gameLoop->init("GameEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
+	while (gameLoop->running())
+	{
+		gameLoop->handleEvents();
+		gameLoop->update();
+		gameLoop->render();
+
+	}
+
+	gameLoop->clean();
 
 	return 0;
 }
