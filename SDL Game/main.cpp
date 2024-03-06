9580d136d5ae2b892bf832d1da1f00d102e57a93
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-06 13:34:19 +0100

    Limited frame rate of program to 60fps

--- SDL Game/main.cpp
+++ SDL Game/main.cpp
@@ -7,15 +7,29 @@ GameLoop* gameLoop = nullptr;
 
 int main(int argc, char* argv[]) 
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	gameLoop = new GameLoop();
 	gameLoop->init("GameEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (gameLoop->running())
 	{
+		frameStart = SDL_GetTicks();
+
 		gameLoop->handleEvents();
 		gameLoop->update();
 		gameLoop->render();
 
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	gameLoop->clean();
