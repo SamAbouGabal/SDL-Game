39dbd06fe21c80e4ca2d8fa630796f7b769f2871
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-03-06 21:40:58 +0100

    Update to GameObject

--- SDL Game/GameObject.cpp
+++ SDL Game/GameObject.cpp
@@ -11,10 +11,17 @@ GameObject::GameObject(const char* textureSheet, SDL_Renderer* ren) {
 void GameObject::Update() {
 
 	xPos = 0;
-	xPos = 0;
+	yPos = 0;
 
 	srcRect.h = 32;
 	srcRect.w = 32;
+	srcRect.x = 0;
+	srcRect.y = 0;
+	
+	destRect.x = xPos;
+	destRect.y = yPos;
+	destRect.w = srcRect.w * 2;
+	destRect.h = srcRect.h * 2; 
 
 }
 
